88167637c66b66bf71635164dc9d17b35f52395b
patrick-0864 <ppss12303@gmail.com>
2025-12-07 02:45:28 +0800

    Change App import from App.js to App.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js'
+import App from './App.jsx'
 
 // 這裡負責將你的 App 顯示在 HTML 的 root 元素中
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -8,3 +8,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
